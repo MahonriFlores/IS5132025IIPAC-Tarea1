785019272683735c18bb0b955e013ab88650f58b
mahonri Flores <mahonri.flores@unah.hn>
2025-06-07 19:49:02 -0600

    solucionando el problem con la variable privada de saldo en el ejercicio 5, ejercicio terminado

--- Ejercicio-05.dart
+++ Ejercicio-05.dart
@@ -7,43 +7,55 @@ c. Consultar el saldo.
 */
 class CuentaBancaria {
   final String titular;
-  double _saldo;
+  double _saldo =0.0;
 
-  CuentaBancaria({required this.titular, this._saldo = 0.0});
-  
-  double get saldo => this._saldo;
-  set saldo(double valor) {
-    if (valor >= 0) {
-      _saldo = valor;
-    } else {
-      print('El saldo no puede ser negativo.');
-    }
+  CuentaBancaria.CuentaBancaria({required this.titular, double saldo = 0.0}) {
+    this.saldo = saldo;
   }
+
+  double get saldo => _saldo;
+  set saldo(double nuevoSaldo) {
+    if (nuevoSaldo < 0) {
+      throw Exception('El saldo no puede ser negativo');
+    }
+    _saldo = nuevoSaldo;
+  }
+  
+  void consultarSaldo() {
+    print('El saldo de $titular es: $saldo');
+  }
+  
 
   void depositar(double cantidad){
     if (cantidad>0){
-      saldo += cantidad;>
-      print('Deposito de $cantidad realizado.');
-      print('Nuevo saldo: $saldo');
+      saldo += cantidad;
+      print('Deposito de $cantidad realizado');
+      //print('Nuevo saldo: $saldo');
     }
     else{
-      print('Valor de deposito es 0 o menor.');
+      print('Valor de deposito es 0 o menor');
     }
   }
 
   void retiro(double cantidad){
     if (cantidad > 0 && cantidad <= saldo){
       saldo -= cantidad;
-      print('Retiro de $cantidad realizado.');
-      print('Nuevo saldo: $saldo');
+      print('Retiro de $cantidad realizado');
+      //print('Nuevo saldo: $saldo');
     } else if (cantidad > saldo) {
-      print('Saldo insuficiente para retirar $cantidad.');
+      print('Saldo insuficiente para retirar $cantidad');
     } else {
-      print('Valor de retiro es 0 o menor.');
+      print('Valor de retiro es 0 o menor');
     }
   }
   
 }
 void main(){
-  
+  CuentaBancaria cuentamaho = CuentaBancaria.CuentaBancaria(titular: 'Maho', saldo: 1000.0);
+  cuentamaho.consultarSaldo();
+  cuentamaho.depositar(500.0);
+  cuentamaho.retiro(200.0);
+  cuentamaho.retiro(1500.0); 
+  cuentamaho.depositar(500.0); 
+  cuentamaho.consultarSaldo();
 }
