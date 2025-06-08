0ad9c89d2f223e0552624eceff2fa719ef2bedd7
mahonri Flores <mahonri.flores@unah.hn>
2025-06-07 19:01:47 -0600

    eliminando un ejercicios ya realizado por sahid

--- Ejercicio-03.dart
+++
@@ -1,8 +0,0 @@
-/*
-Cree una clase empleado que contenga las propiedades nombre, puesto y salario.
-luego cree una lista de empleados y una funcion que calcule el salario total de todos los empleados
-y otra que calcule el salario promedio de los empleados.
-*/
-void main(){
-
-}
